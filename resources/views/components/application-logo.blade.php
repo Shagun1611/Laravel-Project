90e1ea1b2359695c504387c0e9f2d2b1dfac31c1
Shagun Sharma <shagu22222@gmail.com>
2025-04-28 20:46:29 +0530

    Initial commit

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,10 @@
+<!-- <div class="logo w-[12rem]">
+    <a href="{{ route('home') }}"><img src="{{ asset('assets/imgs/logo/logo2.jpeg')}}" class="" alt="logo"></a>
+</div> -->
+
+<div class="logo w-[8rem]">
+    <a href="{{ route('home') }}">
+        <img src="{{ asset('assets/imgs/logo/logo2.jpeg') }}" class="w-[6rem] h-[6rem] rounded-full object-cover" alt="logo">
+    </a>
+</div>
+
